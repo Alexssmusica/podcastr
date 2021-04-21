c510a5c4fc150784522f44c4a0f41780fb8cd43b
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-21 14:24:36 -0300

    Aula 2 NLW#5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/app';
-
-import '../styles/global.css';
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+import styles from '../styles/app.module.scss';
+import '../styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div className={styles.wrapper}>
+            <main>
+                <Header />
+                <Component {...pageProps} />
+            </main>
+            <Player />
+        </div>
+    );
 }
 
 export default App;
