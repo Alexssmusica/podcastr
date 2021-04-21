f7e31bb6a0a9d7359fb5872b2718f0b76ae10889
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-20 22:56:22 -0300

    ajustes arquivos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,9 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 
-import GlobalStyles from 'styles/global';
+import '../styles/global.css';
 
 function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>Podcastr</title>
-                <link rel="shortcut icon" href="/favicon.png" />
-                <link rel="apple-touch-icon" href="/favicon.png" />
-                <link rel="manifest" href="/manifest.json" />
-                <meta
-                    name="description"
-                    content="A simple project starter to work with TypeScript, React, NextJs
-                and Style Components"
-                />
-            </Head>
-            <GlobalStyles />
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
 
 export default App;
