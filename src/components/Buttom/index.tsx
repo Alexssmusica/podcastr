d2302ec2f2bd4a77e8c847b52512cd3d9c07464b
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-20 21:51:22 -0300

    primeiro commit

--- src/components/Buttom/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as Style from './styles';
-
-const Buttom = () => (
-    <Style.Wrapper>
-        <h1>Buttom</h1>
-    </Style.Wrapper>
-);
-
-export default Buttom;
--- src/components/Buttom/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0';
-import Buttom from '.';
-
-export default {
-    title: 'Buttom',
-    component: Buttom
-} as Meta;
-
-export const Basic: Story = (args) => <Buttom {...args} />;
