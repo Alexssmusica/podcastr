a80830f00df11c1505f2e0b756188ba0f0235130
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-25 21:36:06 -0300

    Add  PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        disable: !isProd
+    },
     images: {
         domains: ['storage.googleapis.com']
     }
-}
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="pt-BR">
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                     />
                     <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+                    <link rel="manifest" href="/manifest.json" />
                 </Head>
                 <body>
                     <Main />
