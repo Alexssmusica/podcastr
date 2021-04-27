91bd5e07ff2adec7ef18e20537814c6d4361c7cd
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-04-27 19:45:04 -0300

    Ajustes PWA

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -31,29 +31,31 @@ type EpisodeProps = {
 export default function Episode({ episode }: EpisodeProps) {
     const { play } = usePlayer();
     return (
-        <div className={styles.episode}>
-            <Head>
-                <title>{episode.title}</title>
-            </Head>
-            <div className={styles.thumbnailContainer}>
-                <Link href="/">
-                    <button type="button">
-                        <img src="/arrow-left.svg" alt="left" />
+        <div className={styles.episodeWrapper}>
+            <div className={styles.episode}>
+                <Head>
+                    <title>{episode.title}</title>
+                </Head>
+                <div className={styles.thumbnailContainer}>
+                    <Link href="/">
+                        <button type="button">
+                            <img src="/arrow-left.svg" alt="left" />
+                        </button>
+                    </Link>
+                    <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
+                    <button type="button" onClick={() => play(episode)}>
+                        <img src="/play.svg" alt="play" />
                     </button>
-                </Link>
-                <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
-                <button type="button" onClick={() => play(episode)}>
-                    <img src="/play.svg" alt="play" />
-                </button>
-            </div>
+                </div>
 
-            <header>
-                <h1>{episode.title}</h1>
-                <span>{episode.members}</span>
-                <span>{episode.publishedAt}</span>
-                <span>{episode.durationAsString}</span>
-            </header>
-            <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }} />
+                <header>
+                    <h1>{episode.title}</h1>
+                    <span>{episode.members}</span>
+                    <span>{episode.publishedAt}</span>
+                    <span>{episode.durationAsString}</span>
+                </header>
+                <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }} />
+            </div>
         </div>
     );
 }
